6e6be7e5f878eaa6bda7ec746e0c503ab7cbe1ff
Tanvir Ahmed <tanver018765@gmail.com>
2025-11-01 19:08:27 +0600

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
 import { Config } from '../src/config/index.js';
+import app from './app.js';
 
-console.log(Config);
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error starting server', error);
+    process.exit(1);
+  }
+};
+
+startServer();
