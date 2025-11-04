6de522c51026fb7b287673465178e41c486536cb
Tanvir Ahmed <tanver018765@gmail.com>
2025-11-04 10:59:30 +0600

    write first test

--- test/user/register.spec.ts
+++ test/user/register.spec.ts
@@ -4,29 +4,54 @@ import app from "../../src/app";
 describe("POST /auth/register", () => {
     // jodi sob field dey tahole eita use hobe
     describe("Given all fields", () => {
-        it("should return 201 status", async () => {
-            // AAA
-            // Arrange
-            // Act
-            // Assert
-
+        it("should return the 201 status code", async () => {
             // Arrange
             const userData = {
-                firstName: "Tanvir",
-                lastName: "Ahmed",
-                email: "tanvir@gmail.com",
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
                 password: "secret",
             };
-
             // Act
             const response = await request(app)
                 .post("/auth/register")
                 .send(userData);
 
             // Assert
+            expect(response.statusCode).toBe(201);
+        });
+
+        it("should return valid json response", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "secret",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            // Assert application/json utf-8
             expect(
-                (response.header as Record<string, string>)["content-type"],
+                (response.headers as Record<string, string>)["content-type"],
             ).toEqual(expect.stringContaining("json"));
+        });
+
+        it("should persist the user in the database", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "secret",
+            };
+            // Act
+            await request(app).post("/auth/register").send(userData);
+
+            // Assert
         });
     });
 
