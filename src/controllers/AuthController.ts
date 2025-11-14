294a372de12fcd6b7c1cc8d83cc8758df710f2b4
Tanvir Ahmed <tanver018765@gmail.com>
2025-11-14 22:32:58 +0600

    create accessToken successfully

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,8 +1,12 @@
+import fs from "fs";
+import path from "path";
 import { NextFunction, Response } from "express";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
+import { JwtPayload, sign } from "jsonwebtoken";
+import createHttpError from "http-errors";
 
 export class AuthController {
     constructor(
@@ -37,6 +41,50 @@ export class AuthController {
                 password,
             });
             this.logger.info("User has been registered", { id: user.id });
+
+            // Generate the access token and refresh token
+            let privateKey: Buffer;
+            try {
+                privateKey = fs.readFileSync(
+                    path.join(__dirname, "../../certs/private.pem"),
+                );
+            } catch (error) {
+                const err = createHttpError(
+                    500,
+                    "Failed to read the private key",
+                );
+                next(err);
+                return;
+            }
+            const payload: JwtPayload = {
+                sub: String(user.id),
+                role: user.role,
+            };
+            const accessToken = sign(payload, privateKey, {
+                algorithm: "RS256",
+                expiresIn: "1h",
+                issuer: "auth-service",
+            });
+            const refreshToken = sign(payload, privateKey, {
+                algorithm: "RS256",
+                expiresIn: "7d",
+                issuer: "auth-service",
+            });
+
+            res.cookie("accessToken", accessToken, {
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60,
+                sameSite: "strict",
+                secure: false,
+            });
+
+            res.cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 24,
+                sameSite: "strict",
+                secure: false,
+            });
+
             res.status(201).json({ id: user.id });
         } catch (err) {
             next(err);
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -4,6 +4,7 @@ import { User } from "../../src/entity/User";
 import { DataSource } from "typeorm";
 import { AppDataSource } from "../../src/config/data-source";
 import { Roles } from "../../src/constants";
+import { isJwt } from "../utils";
 
 describe("POST /auth/register", () => {
     let connection: DataSource;
@@ -158,6 +159,47 @@ describe("POST /auth/register", () => {
             // Assert
             expect(response.statusCode).toBe(400);
             expect(users).toHaveLength(1);
+        });
+
+        it("should return the access token and refresh token inside a cookie", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "password",
+            };
+
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            // accessToken=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjkzOTA5Mjc2LCJleHAiOjE2OTM5MDkzMzYsImlzcyI6Im1lcm5zcGFjZSJ9.KetQMEzY36vxhO6WKwSR-P_feRU1yI-nJtp6RhCEZQTPlQlmVsNTP7mO-qfCdBr0gszxHi9Jd1mqf-hGhfiK8BRA_Zy2CH9xpPTBud_luqLMvfPiz3gYR24jPjDxfZJscdhE_AIL6Uv2fxCKvLba17X0WbefJSy4rtx3ZyLkbnnbelIqu5J5_7lz4aIkHjt-rb_sBaoQ0l8wE5KzyDNy7mGUf7cI_yR8D8VlO7x9llbhvCHF8ts6YSBRBt_e2Mjg5txtfBaDq5auCTXQ2lmnJtMb75t1nAFu8KwQPrDYmwtGZDkHUcpQhlP7R-y3H99YnrWpXbP8Zr_oO67hWnoCSw; Max-Age=43200; Domain=localhost; Path=/; Expires=Tue, 05 Sep 2023 22:21:16 GMT; HttpOnly; SameSite=Strict
+
+            // Assert
+            let accessToken = null;
+            let refreshToken = null;
+            interface Headers {
+                ["set-cookie"]: string[];
+            }
+
+            const cookies = (response.headers as Headers)["set-cookie"] || [];
+
+            cookies.forEach((cookie) => {
+                if (cookie.startsWith("accessToken=")) {
+                    accessToken = cookie.split(";")[0].split("=")[1];
+                }
+                if (cookie.startsWith("refreshToken=")) {
+                    refreshToken = cookie.split(";")[0].split("=")[1];
+                }
+            });
+
+            expect(accessToken).not.toBe(null);
+            expect(refreshToken).not.toBe(null);
+
+            expect(isJwt(accessToken)).toBeTruthy();
+            expect(isJwt(refreshToken)).toBeTruthy();
         });
     });
     describe("Fields are missing", () => {
